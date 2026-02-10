349ca7205a08969f8f2a7aea521318a5c0ede5ef
giovannimazzi <gioelor@libero.it>
2026-02-10 16:24:11 +0100

    step 2: Milestone 1 raggiunta

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import articles from "./data/articles";
 
-import { GiPowerButton } from "react-icons/gi";
+import { BsGeoAltFill } from "react-icons/bs";
 
 export default function App() {
-  {
-    console.log(articles);
-  }
   return (
-    <div className="p-3">
-      <h1>HELLO WORLD!!!</h1>
-      <button className="btn btn-primary d-flex align-items-center gap-2">
-        <GiPowerButton className="text-warning fw-bold" />
-        BUTTON
-      </button>
-    </div>
+    <>
+      <header>
+        <div className="container text-center py-2">
+          <h1>BLOG di Viaggio</h1>
+        </div>
+      </header>
+      <main>
+        <div className="container py-4">
+          {articles.map(({ id, title }) => {
+            return (
+              <div key={id} className="card my-4 bg-transparent rounded-5">
+                <div className="card-body">
+                  <div className="card-title">
+                    <h2 className="h1">
+                      <a href="#" className="text-decoration-none">
+                        <BsGeoAltFill className="me-2 trip-icon" />
+                        <span className="text-dark">{title}</span>
+                      </a>
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </>
   );
 }
